15cc3d0994d83b7d9cdc279d57d8da465e489874
имя_пользователя_github <arseni_p@tut.by>
2019-02-25 16:56:54 +0300

    complete

--- popup.js
+++ popup.js
@@ -1,7 +1,22 @@
 var write = document.querySelector(".write-us");
 var writeForm = document.querySelector(".modal");
+var formClose = writeForm.querySelector(".modal-close");
 
 write.addEventListener("click", function (evt) {
     evt.preventDefault();
     writeForm.classList.add("modal-visual")
-})
+});
+
+formClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    writeForm.classList.remove("modal-visual")
+});
+
+window.addEventListener("keydown", function (evt) {
+    evt.preventDefault();
+    if (evt.keyCode === 27) {
+        if (writeForm.classList.contains("modal-visual")) {
+            writeForm.classList.remove("modal-visual");
+        }
+    }
+});
